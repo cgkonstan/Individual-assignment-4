dfc61cb68b719639397e10a5dd2a0bb8e354bd00
cgkonstan <cgkonstan@gmail.com>
2022-04-14 18:07:12 -0500

    Changed the description

--- script.js
+++ script.js
@@ -27,6 +27,9 @@ function changeImage() {
 function restart() {    
     var image = document.getElementById('ui-img');
     var nextBtn = document.getElementById('next-btn');
+    var restartBtn = document.getElementById('restart-flow');
+    
     image.src = "images/incoming-call.png";
     nextBtn.style.display = "block";
+    restartBtn.style.display ="none";
 }
