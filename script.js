e7c55dc845930971e59938aba49de73f7e094863
cgkonstan <cgkonstan@gmail.com>
2022-04-14 15:50:56 -0500

    set up button maps and added a restart flow

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function changeImage() {
+    var image = document.getElementById('ui-img');
+    var nextBtn = document.getElementById('next-btn')
+    var restartBtn = document.getElementById('restart-flow')
+
+    if (image.src.match("incoming-call")) {
+        image.src = "images/call-declined.png";
+    }
+    else {
+        image.src = "images/start-page.png";
+    }
+
+    if (image.src.match("start-page")) {
+        nextBtn.style.display = "none";
+        restartBtn.style.display ="block"
+    }
+    else {
+        nextBtn.style.display = "block";
+        restartBtn.style.display ="none"
+    }
+}
